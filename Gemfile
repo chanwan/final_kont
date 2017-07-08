da19e80ab43b4c4ac7ffc1977bfefdaafc9468c9
Dair <dairmakeshov@gmail.com>
2017-07-08 17:20:20 +0600

    Установил симпле форм и создал рабочию форму для создание книги и автора

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'coffee-rails', '~> 4.2'
 
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'simple_form'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,23 @@ class BooksController < ApplicationController
 	def index
 		@books = Book.all
 	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :author_id, :category_id, :desc, :image_id, :user_id)
+	end
 end
